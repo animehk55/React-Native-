176b50f44e9ae81dd6a45f8ce2c542984bc56547
SOF <animeshk55@gmail.com>
2018-10-27 20:01:03 +0530

    modificaion in AlbumList page

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
  const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -26,31 +27,32 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Header headerText={'Hello World'} />
-        <Text style={styles.welcome}>Homepage</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Header headerText={'Album'} />
+        <AlbumList />
+        {/* <Text style={styles.welcome}>Homepage</Text>
+        <Text style={styles.instructions}>{instructions}</Text> */}
       </View>
     );
   }
 }
  const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    marginTop: 0 
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: 'white',
+    // marginTop: 0 
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: 'pink'
+    // fontSize: 20,
+    // textAlign: 'center',
+    // margin: 10,
+    // color: 'pink'
   },
   instructions: {
-    textAlign: 'center',
-    color: 'green',
-    marginBottom: 5,
+    // textAlign: 'center',
+    // color: 'green',
+    // marginBottom: 5,
   },
 });
 
