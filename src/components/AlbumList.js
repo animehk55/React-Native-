c2d877c1acec485e891e2a88400ed49e6ebfa9a4
SOF <animeshk55@gmail.com>
2018-10-27 20:21:34 +0530

    Added noted for componentWillMount and more

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+    componentWillMount() {
+        console.log("ComponentWillMount in Album List")
+    }
+
     render() {
     return (
         <View>
