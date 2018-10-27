f6c83ce20b486cafb994e5f9f5c66de3d316ea64
SOF <animeshk55@gmail.com>
2018-10-27 10:57:20 +0530

    cleared all commits

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Header from './src/components/header';
+
  const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -11,6 +13,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <Header />
         <Text style={styles.welcome}>Homepage</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
