6b210fa504c5c68bd2f6824f3a3aefa6eb12baf1
SOF <animeshk55@gmail.com>
2018-10-28 07:50:20 +0530

    Added axios for data fetch and is working too

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
     componentWillMount() {
-        console.log("ComponentWillMount in Album List");
-        debugger;
+        // console.log("ComponentWillMount in Album List");
+        // debugger;
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log(response));
     }
 
     render() {
