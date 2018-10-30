0e33055742eba2f1a88942aeee5ef7ebb4ac9174
SOF <animeshk55@gmail.com>
2018-10-30 08:59:47 +0530

    Added AlbumDetail component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    View,
+    Text
+        } from 'react-native';
+
+
+const AlbumDetail = (props) => {
+    return (
+        <View>
+            <Text>
+                {props.album.title}
+            </Text>
+        </View>
+    );
+};
+
+export default AlbumDetail;
