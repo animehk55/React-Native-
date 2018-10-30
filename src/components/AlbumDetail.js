04066fa55f6372632452a3aaa61eba7adc1191ba
SOF <animeshk55@gmail.com>
2018-10-31 01:47:34 +0530

    Added and displayed the content of API on screen and is working fine also displaying the image as required using height and idth

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,21 +1,74 @@
 import React from 'react';
 import {
-        Text
+        Text,
+        View,
+        Image
         } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image } = album;
+    const { 
+        thumbnailStyle, 
+        headerContentStyle,
+        thumbnailContainerStyle,
+        headerTextStyle,
+        imageStyle
+         } = styles;
+
     return (
         <Card>
             <CardSection>
-                <Text>
-                    {props.album.title}
+                <View style={thumbnailContainerStyle}>
+                    <Image 
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }} 
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                <Text style={headerTextStyle}>
+                    {title}
                 </Text>
+                <Text>
+                    {artist}
+                </Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image
+                    style={imageStyle} 
+                    source={{ uri: image }} 
+                />
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 25
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        flex: 1,
+        width: null,    // 100%
+        height: 300
+    }
 };
 
 export default AlbumDetail;
